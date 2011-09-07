a6def184a57b2218ba062cf942dca28ad692a123
Kevin Reid <kpreid@switchb.org>
2011-09-07 13:11:29 -0400

    Add support for non-drawn blocks, and create a (sketchy) terrain.

--- world.js
+++ world.js
@@ -1,13 +1,13 @@
 function World() {
-  var wx = 16;
+  var wx = 64;
   var wy = 16;
-  var wz = 16;
+  var wz = 64;
   var blocks = new Uint8Array(wx*wy*wz);
   
-  for (var x = 0; x < 16; x++)
-  for (var y = 0; y < 16; y++)
-  for (var z = 0; z < 16; z++) {
-    blocks[x*wy*wz + y*wz + z] = mod(x*y*z, 256);
+  for (var x = 0; x < wx; x++)
+  for (var y = 0; y < wy; y++)
+  for (var z = 0; z < wz; z++) {
+    blocks[x*wy*wz + y*wz + z] = (wy-y)*70 - ((x-wx/2)*(x-wx/2) + (z-wz/2)*(z-wz/2)) > 0 ? 255 : 0;
   }
   
   blocks.wx = wx;
