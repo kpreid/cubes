eec87acd7761d7fd694421e0d5047eb1ea9e1db5
Kevin Reid <kpreid@switchb.org>
2011-09-08 15:01:39 -0400

    Chunked rendering, terrain gen.

--- world.js
+++ world.js
@@ -1,15 +1,15 @@
 function World() {
   "use strict";
 
-  var wx = 32;
+  var wx = 256;
   var wy = 16;
-  var wz = 32;
+  var wz = 256;
   var blocks = new Uint8Array(wx*wy*wz);
   
   for (var x = 0; x < wx; x++)
   for (var y = 0; y < wy; y++)
   for (var z = 0; z < wz; z++) {
-    blocks[x*wy*wz + y*wz + z] = (wy-y)*70 - ((x-wx/2)*(x-wx/2) + (z-wz/2)*(z-wz/2)) > 0 ? 255 : 0;
+    blocks[x*wy*wz + y*wz + z] = (Math.sin(x/10) + Math.sin(z/10))*3 > (y-wy/2) ? Math.sin(x/4) + 1 * 126 : 0;
   }
   
   // --- Internal functions ---
