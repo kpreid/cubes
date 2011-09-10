50cdd9664223caeb5597d67a6dadd5904a6ec6dd
Kevin Reid <kpreid@switchb.org>
2011-09-10 13:01:17 -0400

    More optimization.

--- util.js
+++ util.js
@@ -20,6 +20,7 @@ function signum(x) {
 }
 
 function fixedmultiplyVec3(matrix, vector) {
+  "use strict";
   // glMatrix's multiplyVec3 doesn't work if the implicit fourth OUTPUT is not 1, so doesn't work for matrices such as inverted projection matrices
   var four = [vector[0], vector[1], vector[2], 1];
   mat4.multiplyVec4(matrix, four);
@@ -28,3 +29,11 @@ function fixedmultiplyVec3(matrix, vector) {
   vector[2] = four[2]/four[3];
   return vector;
 }
+
+var UNIT_PX = vec3.create([1,0,0]);
+var UNIT_PY = vec3.create([0,1,0]);
+var UNIT_PZ = vec3.create([0,0,1]);
+var UNIT_NX = vec3.create([-1,0,0]);
+var UNIT_NY = vec3.create([0,-1,0]);
+var UNIT_NZ = vec3.create([0,0,-1]);
+
