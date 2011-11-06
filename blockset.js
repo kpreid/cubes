97bcc230adb90973e91d944a8b27d4e358c6a129
Kevin Reid <kpreid@switchb.org>
2011-11-05 22:54:50 -0400

    Block texture optimization: slice once per dimension.
    Redundant slices were being generated, and CPU was being wasted, by sweeping the block from all six directions. Instead, we slice it in each of three dimensions and use one slice for both faces of the subcubes.

--- blockset.js
+++ blockset.js
@@ -131,7 +131,7 @@ var BlockSet = (function () {
       tileAllocMap = new Uint8Array(tileCountSqrt*tileCountSqrt);
       freePointer = 0;
       
-      // table mapping block slices to tile indexes, format 'worldindex,facename,layerindex'
+      // table mapping block slices to tile indexes, format 'worldindex,dimName,layerindex'
       usageMap = {};
       
       // Flag indicating reallocation
@@ -227,10 +227,11 @@ var BlockSet = (function () {
         }
         
         TILE_MAPPINGS.forEach(function (m) {
-          var faceName = m[0];
+          var dimName = m[0];
           var transform = m[1];
           var layers = [];
-          tilings[blockID][faceName] = layers;
+          tilings[blockID]["l" + dimName] = layers;
+          tilings[blockID]["h" + dimName] = layers;
           for (var layer = 0; layer < World.TILE_SIZE; layer++) {
             // u,v coordinates of this tile for use by the vertex generator
             layers[layer] = layer == 0 ? texgen.uvFor(usageIndex) : null;
@@ -243,56 +244,68 @@ var BlockSet = (function () {
           
           // To support non-cubical objects, we slice the entire volume of the block and generate as many tiles as needed. sliceWorld generates one such slice.
           
-          function sliceWorld(faceName, layer, transform, layers) {
-            var usageIndex = [blockID,faceName,layer].toString();
+          function sliceWorld(dimName, layerL, transform, layersL, layersH) {
+            var layerH = World.TILE_SIZE - 1 - layerL;
+            var usageIndex = [blockID,dimName,layerL].toString();
             
             var coord = texgen.allocationFor(usageIndex);
             var tileu = coord[0], tilev = coord[1];
             
-            var thisLayerNotEmpty = false;
+            var thisLayerNotEmptyL = false;
+            var thisLayerNotEmptyH = false;
             var pixu = tileu*World.TILE_SIZE;
             var pixv = tilev*World.TILE_SIZE;
             // extract surface plane of block from world
             for (var u = 0; u < World.TILE_SIZE; u++)
             for (var v = 0; v < World.TILE_SIZE; v++) {
               var c = ((pixu+u) * blockTextureData.width + pixv+v) * 4;
-              var vec = vec3.create([u,v,layer]);
+              var vec = vec3.create([u,v,layerL]);
               mat4.multiplyVec3(transform, vec, vec);
-              var view = vec3.create([u,v,layer-1]);
-              mat4.multiplyVec3(transform, view, view);
+              var viewL = vec3.create([u,v,layerL-1]);
+              mat4.multiplyVec3(transform, viewL, viewL);
+              var viewH = vec3.create([u,v,layerL+1]);
+              mat4.multiplyVec3(transform, viewH, viewH);
           
               var type = world.gt(vec[0],vec[1],vec[2]);
               type.writeColor(255, blockTextureData.data, c);
               if (blockTextureData.data[c+3] < 255) {
                 // A block is opaque if all of its outside (layer-0) pixels are opaque.
-                if (layer == 0)
+                if (layerL == 0 || layerH == 0)
                   opaque = false;
-              } else if (!world.opaque(view[0],view[1],view[2])) {
-                // A layer has significant content only if there is an UNOBSCURED (hence the above check) opaque pixel.
-                thisLayerNotEmpty = true;
+              } else {
+                // A layer has significant content only if there is an UNOBSCURED opaque pixel.
+                // If a layer is "empty" in this sense, it is not rendered.
+                // If it is empty from both directions, then it is deallocated.
+                if (!world.opaque(viewL[0],viewL[1],viewL[2])) {
+                  thisLayerNotEmptyL = true;
+                }
+                if (!world.opaque(viewH[0],viewH[1],viewH[2])) {
+                  thisLayerNotEmptyH = true;
+                }
               }
             }
             
-            // We can reuse this tile iff it was blank
-            if (!thisLayerNotEmpty) {
+            // We can reuse this tile iff it was blank or fully obscured
+            if (!thisLayerNotEmptyL && !thisLayerNotEmptyH) {
               texgen.deallocateUsage(usageIndex);
             }
             
             // u,v coordinates of this tile for use by the vertex generator
-            layers[layer] = thisLayerNotEmpty ? texgen.uvFor(usageIndex) : null;
+            layersL[layerL] = thisLayerNotEmptyL ? texgen.uvFor(usageIndex) : null;
+            layersH[layerH] = thisLayerNotEmptyH ? texgen.uvFor(usageIndex) : null;
             
             // TODO: trigger rerender of chunks only if we made changes to the tiling, not if only the colors changed
             
-            //console.log("id ", wi + 1, " face ", faceName, " layer ", layer, thisLayerNotEmpty ? " allocated" : " skipped");
+            //console.log("id ", wi + 1, " dim ", dimName, " layer ", layer, (thisLayerNotEmptyL || thisLayerNotEmptyH) ? " allocated" : " skipped");
           }
           TILE_MAPPINGS.forEach(function (m) {
-            var faceName = m[0];
+            var dimName = m[0];
             var transform = m[1];
-            var layers = [];
-            tilings[blockID][faceName] = layers;
+            var layersL = tilings[blockID]["l" + dimName] = [];
+            var layersH = tilings[blockID]["h" + dimName] = [];
             for (var layer = 0; layer < World.TILE_SIZE; layer++) {
               if (texgen.textureLost) return;
-              sliceWorld(faceName, layer, transform, layers);
+              sliceWorld(dimName, layer, transform, layersL, layersH);
             }
             type.opaque = opaque; // TODO have blocktype itself handle this
           });
@@ -379,47 +392,26 @@ var BlockSet = (function () {
     // in this matrix layout, the input (column) vector is the tile coords
     // and the output (row) vector is the world space coords
     // so the lower row is the translation component.
-    ["lz", mat4.create([
+    ["z", mat4.create([
       // low z face
       1, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 1, 0,
       0, 0, 0, 1
     ])],
-    ["hz", mat4.create([
-      // high z face
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, -1, 0,
-      0, 0, 15, 1
-    ])],
-    ["lx", mat4.create([
+    ["x", mat4.create([
       // low x face
       0, 1, 0, 0,
       0, 0, 1, 0,
       1, 0, 0, 0,
       0, 0, 0, 1
     ])],
-    ["hx", mat4.create([
-      // high x face
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      -1, 0, 0, 0,
-      15, 0, 0, 1
-    ])],
-    ["ly", mat4.create([
+    ["y", mat4.create([
       // low y face
       0, 0, 1, 0,
       1, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 0, 1
-    ])],
-    ["hy", mat4.create([
-      // high y face
-      0, 0, 1, 0,
-      1, 0, 0, 0,
-      0, -1, 0, 0,
-      0, 15, 0, 1
     ])],
   ];
   
