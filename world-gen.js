196611c25c9ef6601e917b530de126e0dfb05b14
Kevin Reid <kpreid@switchb.org>
2011-11-07 19:07:29 -0500

    Refactor block world generation.
    * Extract "generate a world block type" into WorldGen method.
    * Make default block world generation less dependent on absolute indexes.

--- world-gen.js
+++ world-gen.js
@@ -3,8 +3,14 @@
 
 var WorldGen = (function () {
   "use strict";
+
+  var blockWorldSize = [World.TILE_SIZE,World.TILE_SIZE,World.TILE_SIZE];
   
   var WorldGen = {
+    newWorldBlockType: function (blockSet) {
+       return new BlockType.World(new World(blockWorldSize, blockSet));
+    },
+    
     // Generate a blockset containing RGB colors with the specified number of
     // levels in each channel, and a function from (r,g,b) to block ID.
     colorBlocks: function (reds, greens, blues) {
@@ -65,14 +71,8 @@ function generateWorlds() {
   var colorSet = colors.blockset;
   var brgb = colors.colorToID;
 
-  // --- block worlds ---
+  // --- block world generation utilities ---
   
-  var blockWorldSize = [World.TILE_SIZE,World.TILE_SIZE,World.TILE_SIZE];
-  var blockWorldCount = 8;
-  var types = [];
-  for (var i = 0; i < blockWorldCount; i++)
-    types.push(new BlockType.World(new World(blockWorldSize, colorSet)));
-
   // condition functions for procedural block generation
   // Takes a coordinate vector and returns a boolean.
   // TODO: Parameterize on TILE_SIZE.
@@ -113,61 +113,64 @@ function generateWorlds() {
     };
   }
   
-  function genedit(world, patfunc) {
-    world.edit(function (x,y,z,value) {
+  function genedit(patfunc) {
+    var type = WorldGen.newWorldBlockType(colorSet);
+    type.world.edit(function (x,y,z,value) {
       return patfunc([x,y,z]);
     });
+    return type;
   }
-  
-  // color cube - world base
-  genedit(types[0].world, function (b) {
+
+  // --- default block worlds and block set ---
+
+  var type;
+  var blockset = new BlockSet([]);
+
+  // color cube - world base and bogus-placeholder
+  blockset.add(type = genedit(function (b) {
     return rgbPat(b);
-  });
+  }));
   
   // ground block
-  genedit(types[1].world, function (b) {
+  blockset.add(type = genedit(function (b) {
     return (te(b) ? speckle(flat(brgb(.67,.34,.34)), flat(brgb(.67,0,0))) :
             tp(b) ? flat(brgb(1,.34,.34)) :
             speckle(flat(brgb(.34,0,0)), flat(brgb(0,0,0))))(b);
-  });
-  types[1].spontaneousConversion = 2+1;
+  }));
+  type.spontaneousConversion = 3;
   
   // ground block #2
-  genedit(types[2].world, function (b) {
+  blockset.add(type = genedit(function (b) {
     return (te(b) ? speckle(flat(brgb(.34,.67,.34)), flat(brgb(0,.34,0))) :
             tp(b) ? flat(brgb(.34,1,.34)) :
             speckle(flat(brgb(0,.34,0)), flat(brgb(0,1,1))))(b);
-  });
-  types[2].spontaneousConversion = 1+1;
+  }));
+  type.spontaneousConversion = 2;
   
   // pyramid thing
-  genedit(types[3].world, function (b) {
+  blockset.add(type = genedit(function (b) {
     if (Math.abs(b[0] - 7.5) + Math.abs(b[1] - 7.5) > 15.5-b[2])
       return 0;
     return brgb(mod((b[2]+2)/8, 1), Math.floor((b[2]+2)/8)*0.5, 0);
-  });
-  types[3].automaticRotations = sixFaceRotations;
+  }));
+  type.automaticRotations = sixFaceRotations;
   
   // "leaf block" transparency test
-  genedit(types[4].world, function (b) {
+  blockset.add(type = genedit(function (b) {
     return s(b) ? speckle(flat(0), flat(brgb(0,1,0)))(b) : 0;
-  });
+  }));
 
   // pillar thing
-  genedit(types[5].world, function (b) {
+  blockset.add(type = genedit(function (b) {
     return Math.max(Math.abs(b[0] - 8), Math.abs(b[2] - 8)) <= 4 ? brgb(.5,.5,0) : 0;
-  });
+  }));
   
-  for (var i = 6; i < blockWorldCount; i++) {
-    var c = pickCond(flat(pickColor()), 
-              pickCond(flat(pickColor()), 
+  for (var i = 0; i < 4; i++) {
+    var c = pickCond(flat(pickColor()),
+              pickCond(flat(pickColor()),
                 speckle(flat(pickColor()), flat(pickColor()))));
-    genedit(types[i].world, c);
+    blockset.add(genedit(c));
   }
-  
-  // --- main blockset ---
-  
-  var blockset = new BlockSet(types);
   
   // --- big world ---
 
