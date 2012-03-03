9fa0d116cc057bdf1ab6189ecc5a88dcdd1b6e5c
Kevin Reid <kpreid@switchb.org>
2012-03-03 16:02:26 -0500

    Refine main test case.

--- test/test-main.js
+++ test/test-main.js
@@ -1,28 +1,37 @@
+// Copyright 2011-2012 Kevin Reid under the terms of the MIT License as detailed
+// in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
+
 describe("CubesMain", function() {
-  sessionStorage.clear();
+  beforeEach(function () {
+    // We are using sessionStorage as a temporary Storage object for testing.
+    sessionStorage.clear();
+  });
   
   function stubElem() { 
     return document.createElement("div");
   }
-  
-  it("should be instantiable", function() {
-    var main = new CubesMain(1/60, sessionStorage);
-    
-    var parts = {
-      // TODO: Reduce the amount of this boilerplate needed.
-      viewCanvas: document.createElement("canvas"),
-      menu: stubElem(),
-      sceneInfoOverlay: stubElem(),
-      cursorInfoOverlay: stubElem(),
-      loadError: [stubElem(), stubElem()]
-    };
-    
-    var done;
+
+  var parts = {
+    // TODO: Reduce the amount of this boilerplate needed.
+    viewCanvas: document.createElement("canvas"),
+    menu: stubElem(),
+    sceneInfoOverlay: stubElem(),
+    cursorInfoOverlay: stubElem(),
+    loadError: [stubElem(), stubElem()]
+  };
+
+  var main, done;
+  it("should be instantiable", function () {
+    main = new CubesMain(1/60, sessionStorage);
+
     main.start(parts, function (optError) {
       done = [optError];
     });
-    waitsFor(function () { return !!done; }, 10000);    
-    
+  });
+
+  waitsFor(function () { return !!done; }, 10000);    
+  
+  it("should startup successfully", function() {
     // successful startup
     runs(function () {
       expect(done).toEqual([null]);
