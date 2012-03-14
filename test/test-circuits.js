459f81823cbf1892f5434a7284eab325df137784
Kevin Reid <kpreid@switchb.org>
2012-03-14 18:30:22 -0400

    Avoid generating circular world/blockset references in Circuit test.

--- test/test-circuits.js
+++ test/test-circuits.js
@@ -2,35 +2,35 @@
 // in the accompanying file README.md or <http://opensource.org/licenses/MIT>.
 
 describe("Circuit", function() {
-  
   var TS = 16;
-  var blockset, ls, t;
+
+  var t;
   beforeEach(function () {
-    // TODO: This code is duplicative of generateWorlds.
-    //if (!blockset) {
-      // layer 1
-      var pureColors = WorldGen.colorBlocks(4, 4, 4);
-
-      // layer 2
-      var baseLogicAndColors = WorldGen.colorBlocks(4, 4, 4);
-      WorldGen.addLogicBlocks(TS, baseLogicAndColors, pureColors);
-
-      // layer 3
-      blockset = WorldGen.colorBlocks(4, 4, 4);
-      WorldGen.addLogicBlocks(TS, blockset, baseLogicAndColors);
-      ls = {};
-      for (var i = 0; i < blockset.length; i++) {
-        ls[(blockset.get(i).name || "").replace(/^logic\./, "")] = i;
-      }
-    //}
-    
     t = makeTester();
   });
   
   function makeTester() {
+    // TODO: This code is duplicative of generateWorlds.
+    // layer 1
+    var pureColors = WorldGen.colorBlocks(4, 4, 4);
+
+    // layer 2
+    var baseLogicAndColors = WorldGen.colorBlocks(4, 4, 4);
+    WorldGen.addLogicBlocks(TS, baseLogicAndColors, pureColors);
+
+    // layer 3
+    var blockset = WorldGen.colorBlocks(4, 4, 4);
+    WorldGen.addLogicBlocks(TS, blockset, baseLogicAndColors);
+    var ls = {};
+    for (var i = 0; i < blockset.length; i++) {
+      ls[(blockset.get(i).name || "").replace(/^logic\./, "")] = i;
+    }
+
     var self = {
       world: new World([TS, TS, TS], blockset),
       center: [5, 5, 5],
+      blockset: blockset,
+      ls: ls,
 
       // For looking at the test case in-game for debugging.
       dumpWorld: function (name) {
@@ -53,10 +53,10 @@ describe("Circuit", function() {
         if (typeof value !== "number" || value < 0 || value >= 256) {
           throw new Error("Can't yet define a test input value of " + value);
         }
-        self.putNeighbor(offset, ls.constant, value);
+        self.putNeighbor(offset, t.ls.constant, value);
       },
       putOutput: function (offset) {
-        self.putNeighbor(offset, ls.indicator);
+        self.putNeighbor(offset, t.ls.indicator);
       },
       readOutput: function (offset) {
         return self.world.getCircuit(this.center).getBlockOutput(this.center, offset);
@@ -65,31 +65,31 @@ describe("Circuit", function() {
     return self;
   }
 
-  function makeCircuitBlock() {
+  function makeCircuitBlock(t) {
     var inner = makeTester();
     var type = new BlockType.World(inner.world);
     type.behavior = Circuit.behaviors.ic;
-    inner.id = blockset.length;
-    blockset.add(type); // NOTE: This creates a blockset->blocktype->world->blockset circular reference; might be a problem in the future
+    inner.id = t.blockset.length;
+    t.blockset.add(type);
     return inner;
   }
   
   //////////////////////////////////////////////////////////////////////
 
   it("should indicate 0", function () {
-    t.putBlockUnderTest(ls.indicator);
+    t.putBlockUnderTest(t.ls.indicator);
     t.putInput(UNIT_NX, 0);
     expect(t.world.gRot(t.center[0],t.center[1],t.center[2])).toEqual(CubeRotation.identity.code);
   });
 
   it("should indicate 1", function () {
-    t.putBlockUnderTest(ls.indicator);
+    t.putBlockUnderTest(t.ls.indicator);
     t.putInput(UNIT_NX, 2); // any value other than 1 should give the same result
     expect(t.world.gRot(t.center[0],t.center[1],t.center[2])).toEqual(CubeRotation.z180.code);
   });
 
   it("should reevaluate on subdata updates", function () {
-    t.putBlockUnderTest(ls.junction);
+    t.putBlockUnderTest(t.ls.junction);
     t.putInput(UNIT_NX, 0);
     expect(t.readOutput(UNIT_PX)).toEqual(0);
     t.putInput(UNIT_NX, 1);
@@ -98,14 +98,14 @@ describe("Circuit", function() {
 
   it("should have connectivity according to rotations", function () {
     // unrotated gate
-    t.putBlockUnderTest(ls.gate, CubeRotation.identity.code);
+    t.putBlockUnderTest(t.ls.gate, CubeRotation.identity.code);
     t.putInput(UNIT_NX, 2); // connected emitter
     t.putInput(UNIT_NZ, 3); // unconnected emitter
     expect(t.readOutput(UNIT_PX)).toEqual(2);
     expect(t.readOutput(UNIT_PZ)).toBeFalsy();
     
     // rotated case
-    t.putBlockUnderTest(ls.gate, CubeRotation.y270.code);
+    t.putBlockUnderTest(t.ls.gate, CubeRotation.y270.code);
     expect(t.readOutput(UNIT_PX)).toBeFalsy();
     expect(t.readOutput(UNIT_PZ)).toEqual(3);
 
@@ -117,8 +117,8 @@ describe("Circuit", function() {
   
   describe("icOutput", function () {
     it("should cause a block to emit values", function () {
-      var inner = makeCircuitBlock();
-      inner.putBlockUnderTest(ls.icOutput, CubeRotation.identity.code);
+      var inner = makeCircuitBlock(t);
+      inner.putBlockUnderTest(t.ls.icOutput, CubeRotation.identity.code);
       inner.putInput(UNIT_NX, 101);
       inner.putInput(UNIT_PX, 102);
       inner.putInput(UNIT_NY, 103);
@@ -140,7 +140,7 @@ describe("Circuit", function() {
 
   describe("getNeighborID", function () {
     it("should report inner block IDs according to rotation", function () {
-      t.putBlockUnderTest(ls.getNeighborID, CubeRotation.identity.code);
+      t.putBlockUnderTest(t.ls.getNeighborID, CubeRotation.identity.code);
       expect(t.readOutput(UNIT_PX)).toEqual(0);
       t.putNeighbor(UNIT_NZ, 2); // should be irrelevant
       expect(t.readOutput(UNIT_PX)).toEqual(0);
@@ -148,17 +148,17 @@ describe("Circuit", function() {
       expect(t.readOutput(UNIT_PX)).toEqual(3);
 
       // rotated cases
-      t.putBlockUnderTest(ls.getNeighborID, CubeRotation.y270.code);
+      t.putBlockUnderTest(t.ls.getNeighborID, CubeRotation.y270.code);
       expect(t.readOutput(UNIT_PX)).toEqual(2);
     });
 
     it("should report outer block IDs when in a block", function () {
       // Create a block which does the same thing, but on -z axis
-      var inner = makeCircuitBlock();
-      inner.putBlockUnderTest(ls.getNeighborID, CubeRotation.y270.code);
-      inner.putNeighbor(UNIT_PX, ls.icOutput);
-      inner.world.s(0,0,0, ls.getSubDatum);
-      inner.world.s(0,0,1, ls.setRotation);
+      var inner = makeCircuitBlock(t);
+      inner.putBlockUnderTest(t.ls.getNeighborID, CubeRotation.y270.code);
+      inner.putNeighbor(UNIT_PX, t.ls.icOutput);
+      inner.world.s(0,0,0, t.ls.getSubDatum);
+      inner.world.s(0,0,1, t.ls.setRotation);
       
       t.putBlockUnderTest(inner.id, CubeRotation.identity.code);
       expect(t.readOutput(UNIT_PX)).toEqual(0);
@@ -169,6 +169,8 @@ describe("Circuit", function() {
       t.putNeighbor(UNIT_PX, 3);
       t.putBlockUnderTest(inner.id, CubeRotation.y270.code); // rotated 90ª twice, should now read +x
       expect(t.readOutput(UNIT_PZ /* since the output was rotated */)).toEqual(3);
+      
+      t.dumpWorld("getNeighborID rotation test");
     });
   });
 });
